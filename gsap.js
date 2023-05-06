42408ed897c461c9b78e9e7d184792cc9a63e060
altermajor <118938444+Majoralter@users.noreply.github.com>
2023-05-06 21:11:00 +0100

    Added basic functionality

---
+++ gsap.js
@@ -0,0 +1,37 @@
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smoothWheel: true,
+    wheelMultiplier: 20
+})
+
+function raf(time){
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
+
+
+const tl = gsap.timeline({
+    scrollTrigger:{
+        trigger: ".one",
+        pin: true,
+        scrub: true,
+        start: "top top",
+        end: "bottom+=1000px bottom"
+    }
+}), images = gsap.utils.toArray(".img")
+    images.reverse()
+
+images.forEach(image => {
+    tl.to(image,{
+        x: "100vh",
+        y: "50vh",
+        rotate: 90,
+        duration: 8
+    }).to(image,{
+        x: -1500,
+        y: 500,
+        duration: 8
+    })
+})
